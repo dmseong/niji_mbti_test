cb1164099beb7b6f90b1e15cb7b8157a69d08a29
{dmseong} <149465698+dmseong@users.noreply.github.com.>
2025-03-17 21:11:41 +0900

    배치 수정

--- src/pages/start/StartPage.jsx
+++ src/pages/start/StartPage.jsx
@@ -15,7 +15,7 @@ function StartPage() {
             <div className="rectangle"></div>
             <img className="logo" src={process.env.PUBLIC_URL +'/img/Nijisanji_Logo.webp'} />
             <h1 className="title">나에게 잘 맞는</h1>
-            <h1 className="secondTitle">니지산지 라이버 찾아보기</h1>
+            <h1 className="secondTitle">니지산지 라이버<br />찾아보기</h1>
             <form onSubmit={handleSubmit}>
                 <input id="namefield" type="text" value={name} onChange={handleChange} placeholder="이름을 입력하세요" />
                 <button id="start" type="submit" disabled={!name}>Start Test</button>
